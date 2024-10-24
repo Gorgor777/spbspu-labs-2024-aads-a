6a93a0e7aa2ae83c2dd30d5ac89c4b52595bb754
Gorgor777 <sergei-agarkov07@mail.ru>
2024-10-24 15:14:07 +0300

    upgrade and test insert

--- agarkov.sergey/S4/avltree.hpp
+++ agarkov.sergey/S4/avltree.hpp
@@ -16,6 +16,8 @@ namespace agarkov
       void insert(Key key, Value value);
       Tree< data_t >* node_;
       Compare comp_;
+
+      Tree< data_t >* insert(Key key, Value value, Tree< data_t >* tree);
   };
 
   template< typename Key, typename Value, typename Compare >
@@ -25,15 +27,44 @@ namespace agarkov
   {
   }
 
-  template< typename Key, typename Value, typename Compare >
-  void AVLTree< Key, Value, Compare >::insert(Key key, Value value)
+  template<typename Key, typename Value, typename Compare>
+  Tree< typename AVLTree< Key, Value, Compare >::data_t >* AVLTree< Key, Value, Compare >::insert(Key key, Value value, Tree<data_t>* tree)
   {
     if (!node_)
     {
       node_ = new Tree< data_t >();
       node_->data_ = data_t(key, value);
-      return
+      return node_;
     }
+
+    if (comp_(tree->data_.first, key))
+    {
+      if (!tree->right_)
+      {
+        auto temp = new Tree< data_t >();
+        temp->data_ = data_t(key, value);
+        tree->right_ = temp;
+        return tree->right_;
+      }
+      return insert(key, value, node_->right_);
+    }
+    else
+    {
+      if (!tree->left_)
+      {
+        auto temp = new Tree< data_t >();
+        temp->data_ = data_t(key, value);
+        tree->left_ = temp;
+        return tree->left_;
+      }
+      return insert(key, value, node_->left_);
+    }
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  void AVLTree< Key, Value, Compare >::insert(Key key, Value value)
+  {
+    insert(key, value, node_);
   }
 
 }
--- agarkov.sergey/S4/main.cpp
+++ agarkov.sergey/S4/main.cpp
@@ -1,4 +1,31 @@
-int main() 
-{
+#include <iostream>
+#include "avltree.hpp"
 
+
+template<typename Key, typename Value>
+void inOrderTraversal(Tree<std::pair<Key, Value>>* node)
+{
+  if (node)
+  {
+    inOrderTraversal(node->left_);
+    std::cout << node->data_.first << " " << node->data_.second << "\n";
+    inOrderTraversal(node->right_);
+  }
+}
+
+int main()
+{
+  agarkov::AVLTree<int, std::string> avlTree;
+  avlTree.insert(5, "five");
+  avlTree.insert(3, "three");
+  avlTree.insert(7, "seven");
+  avlTree.insert(2, "two");
+  avlTree.insert(4, "four");
+  avlTree.insert(6, "six");
+  avlTree.insert(8, "eight");
+
+
+  inOrderTraversal(avlTree.node_);
+  std::cout << "\n";
+  return 0;
 }
