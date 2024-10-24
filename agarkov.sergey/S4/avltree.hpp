beec6e7ffaab3d0a7e5d1d632cb7eeabfd302caa
Gorgor777 <sergei-agarkov07@mail.ru>
2024-10-24 15:38:10 +0300

    updateHeight and erase

--- agarkov.sergey/S4/avltree.hpp
+++ agarkov.sergey/S4/avltree.hpp
@@ -13,11 +13,14 @@ namespace agarkov
     public:
       using data_t = std::pair< Key, Value >;
       AVLTree();
-      void insert(Key key, Value value);
+      void insert(const Key& key, const Value& value);
+      void erase(const Key& key);
       Tree< data_t >* node_;
       Compare comp_;
-
-      Tree< data_t >* insert(Key key, Value value, Tree< data_t >* tree);
+    private:
+      Tree< data_t >* insert(const Key& key, const Value& value, Tree< data_t >* tree);
+      Tree< data_t >* find(const Key& key, Tree< data_t >* tree);
+      void erase(Tree< data_t >* tree);
   };
 
   template< typename Key, typename Value, typename Compare >
@@ -27,26 +30,43 @@ namespace agarkov
   {
   }
 
+  template< typename Key, typename Value, typename Compare >
+  void AVLTree< Key, Value, Compare >::insert(const Key& key, const Value& value)
+  {
+    insert(key, value, node_);
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  void AVLTree< Key, Value, Compare >::erase(const Key& key)
+  {
+    erase(find(key, node_));
+  }
+
   template<typename Key, typename Value, typename Compare>
-  Tree< typename AVLTree< Key, Value, Compare >::data_t >* AVLTree< Key, Value, Compare >::insert(Key key, Value value, Tree<data_t>* tree)
+  Tree< typename AVLTree< Key, Value, Compare >::data_t >* AVLTree< Key, Value, Compare >::insert(const Key& key,
+      const Value & value, Tree< data_t >* tree)
   {
     if (!node_)
     {
       node_ = new Tree< data_t >();
       node_->data_ = data_t(key, value);
+      node_->height_ = 1;
       return node_;
     }
-
     if (comp_(tree->data_.first, key))
     {
       if (!tree->right_)
       {
         auto temp = new Tree< data_t >();
         temp->data_ = data_t(key, value);
+        temp->height_ = 1;
         tree->right_ = temp;
+        tree->height_ = 1 + std::max(getHeight(tree->left_), getHeight(tree->right_));
         return tree->right_;
       }
-      return insert(key, value, node_->right_);
+      auto temp = insert(key, value, tree->right_);
+      tree->height_ = 1 + std::max(getHeight(tree->left_), getHeight(tree->right_));
+      return temp;
     }
     else
     {
@@ -54,19 +74,54 @@ namespace agarkov
       {
         auto temp = new Tree< data_t >();
         temp->data_ = data_t(key, value);
+        temp->height_ = 1;
         tree->left_ = temp;
+        tree->height_ = 1 + std::max(getHeight(tree->left_), getHeight(tree->right_));
         return tree->left_;
       }
-      return insert(key, value, node_->left_);
+      auto temp = insert(key, value, tree->left_);
+      tree->height_ = 1 + std::max(getHeight(tree->left_), getHeight(tree->right_));
+      return temp;
     }
   }
 
   template< typename Key, typename Value, typename Compare >
-  void AVLTree< Key, Value, Compare >::insert(Key key, Value value)
+  Tree< typename AVLTree< Key, Value, Compare >::data_t >* AVLTree< Key, Value, Compare >::find(const Key& key, Tree< data_t >* tree)
   {
-    insert(key, value, node_);
+    if (!tree)
+    {
+      return nullptr;
+    }
+    if (comp_(key, tree->data_.first))
+    {
+      if (key == tree->data_.first)
+      {
+        return tree;
+      }
+      return find(key, tree->right_);
+    }
+    else
+    {
+      if (key == tree->data_.first)
+      {
+        return tree;
+      }
+      return find(key, tree->left_);
+    }
   }
 
+  template< typename Key, typename Value, typename Compare >
+  void AVLTree< Key, Value, Compare >::erase( Tree< typename AVLTree<Key, Value, Compare >::data_t >* tree)
+  {
+    if (!tree)
+    {
+      return;
+    }
+    if (!tree->right_ || !tree->left_)
+    {
+      tree = (tree->left_ = nullptr) ? tree->left_ : tree->right_;
+    }
+  }
 }
 
 #endif
--- agarkov.sergey/S4/tree.hpp
+++ agarkov.sergey/S4/tree.hpp
@@ -5,120 +5,47 @@
 
 namespace agarkov
 {
-  template< typename Key, typename Value, typename Compare = std::less< > >
-class AVLTree
-{
-  public:
-    using data_t = std::pair< Key, Value >;
-    AVLTree();
-    void insert(const Key& key, const Value& value);
-    void erase(const Key& key);
-    Tree< data_t >* node_;
-    Compare comp_;
-  private:
-    Tree< data_t >* insert(const Key& key, const Value& value, Tree< data_t >* tree);
-    Tree< data_t >* find(const Key& key, Tree< data_t >* tree);
-    void erase(Tree< data_t >* tree);
-};
-
-template< typename Key, typename Value, typename Compare >
-AVLTree< Key, Value, Compare >::AVLTree():
-  node_(nullptr),
-  comp_()
-{
-}
-
-template< typename Key, typename Value, typename Compare >
-void AVLTree< Key, Value, Compare >::insert(const Key& key, const Value& value)
-{
-  insert(key, value, node_);
-}
-
-template< typename Key, typename Value, typename Compare >
-void AVLTree< Key, Value, Compare >::erase(const Key& key)
-{
-  erase(find(key, node_));
-}
-
-template<typename Key, typename Value, typename Compare>
-Tree< typename AVLTree< Key, Value, Compare >::data_t >* AVLTree< Key, Value, Compare >::insert(const Key& key,
-    const Value & value, Tree< data_t >* tree)
-{
-  if (!node_)
+  template< typename T >
+  struct Tree
   {
-    node_ = new Tree< data_t >();
-    node_->data_ = data_t(key, value);
-    node_->height_ = 1;
-    return node_;
+    T data_;
+    Tree< T >* left_ = nullptr;
+    Tree< T >* head_ = nullptr;
+    Tree< T >* right_ = nullptr;
+    size_t height_ = 0;
+  };
+
+  template< typename T >
+  size_t getHeight(const Tree< T >* tree)
+  {
+    if (tree)
+    {
+      return tree->height_;
+    }
+    return 0ull;
   }
-  if (comp_(tree->data_.first, key))
+
+  template< typename T >
+  Tree< T >* getMax(Tree< T >* tree)
   {
     if (!tree->right_)
     {
-      auto temp = new Tree< data_t >();
-      temp->data_ = data_t(key, value);
-      temp->height_ = 1;
-      tree->right_ = temp;
-      tree->height_ = 1 + std::max(getHeight(tree->left_), getHeight(tree->right_));
-      return tree->right_;
+      return tree;
     }
-    auto temp = insert(key, value, tree->right_);
-    tree->height_ = 1 + std::max(getHeight(tree->left_), getHeight(tree->right_));
-    return temp;
+    return getMax(tree->right_);
   }
-  else
+
+  template< typename T >
+  Tree< T >* getMin(Tree< T >* tree)
   {
     if (!tree->left_)
     {
-      auto temp = new Tree< data_t >();
-      temp->data_ = data_t(key, value);
-      temp->height_ = 1;
-      tree->left_ = temp;
-      tree->height_ = 1 + std::max(getHeight(tree->left_), getHeight(tree->right_));
-      return tree->left_;
-    }
-    auto temp = insert(key, value, tree->left_);
-    tree->height_ = 1 + std::max(getHeight(tree->left_), getHeight(tree->right_));
-    return temp;
-  }
-}
-
-template< typename Key, typename Value, typename Compare >
-Tree< typename AVLTree< Key, Value, Compare >::data_t >* AVLTree< Key, Value, Compare >::find(const Key& key, Tree< data_t >* tree)
-{
-  if (!tree)
-  {
-    return nullptr;
-  }
-  if (comp_(key, tree->data_.first))
-  {
-    if (key == tree->data_.first)
-    {
       return tree;
     }
-    return find(key, tree->right_);
+    return getMax(tree->left_);
   }
-  else
-  {
-    if (key == tree->data_.first)
-    {
-      return tree;
-    }
-    return find(key, tree->left_);
-  }
-}
 
-template< typename Key, typename Value, typename Compare >
-void AVLTree< Key, Value, Compare >::erase( Tree< typename AVLTree<Key, Value, Compare >::data_t >* tree)
-{
-  if (!tree)
-  {
-    return;
-  }
-  if (!tree->right_ || !tree->left_)
-  {
-    tree = (tree->left_ = nullptr) ? tree->left_ : tree->right_;
-  }
+
 }
 
 #endif
