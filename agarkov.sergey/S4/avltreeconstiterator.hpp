25e44a707843e4bffa1559c4839193ab0c5163de
Gorgor777 <sergei-agarkov07@mail.ru>
2024-10-26 22:30:15 +0300

    const iterator constructor

--- agarkov.sergey/S4/avltreeconstiterator.hpp
+++ agarkov.sergey/S4/avltreeconstiterator.hpp
@@ -1,14 +1,36 @@
 #ifndef AVLTREECONSTITERATOR
 #define AVLTREECONSTITERATOR
 
+#include <utility>
+
 namespace agarkov
 {
+  template< typename T >
+  class Tree;
+
+  template< typename Key, typename Value, typename Compare >
+  class AVLTree;
+
+  template< typename Key, typename Value, typename Compare >
+  class AVLTreeConstIterator;
+
+  template< typename Key, typename Value, typename Compare >
   class AVLTreeConstIterator
   {
     public:
+      using data_t = std::pair< Key, Value >;
       AVLTreeConstIterator();
       ~AVLTreeConstIterator() = default;
+    private:
+      Tree< data_t >* ptr_;
   };
+
+  template< typename Key, typename Value, typename Compare >
+  AVLTreeConstIterator< Key, Value, Compare >::AVLTreeConstIterator():
+    ptr_(nullptr)
+  {
+  }
+
 }
 
 #endif
