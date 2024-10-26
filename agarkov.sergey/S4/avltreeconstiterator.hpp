76a0d22398b2bee863f6b1166b74dceaa1e71c23
Gorgor777 <sergei-agarkov07@mail.ru>
2024-10-26 22:36:42 +0300

    const iterator to iterator

--- agarkov.sergey/S4/avltreeconstiterator.hpp
+++ agarkov.sergey/S4/avltreeconstiterator.hpp
@@ -12,14 +12,18 @@ namespace agarkov
   class AVLTree;
 
   template< typename Key, typename Value, typename Compare >
-  class AVLTreeConstIterator;
+  class AVLTreeIterator;
 
   template< typename Key, typename Value, typename Compare >
   class AVLTreeConstIterator
   {
+    friend class AVLTree< Key, Value, Compare >;
+    friend class AVLTreeIterator< Key, Value, Compare >;
     public:
       using data_t = std::pair< Key, Value >;
       AVLTreeConstIterator();
+      explicit AVLTreeConstIterator(Tree< data_t >* rhs);
+      AVLTreeConstIterator(const AVLTreeIterator< Key, Value, Compare >& rhs);
       ~AVLTreeConstIterator() = default;
     private:
       Tree< data_t >* ptr_;
@@ -28,6 +32,18 @@ namespace agarkov
   template< typename Key, typename Value, typename Compare >
   AVLTreeConstIterator< Key, Value, Compare >::AVLTreeConstIterator():
     ptr_(nullptr)
+  {
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  AVLTreeConstIterator< Key, Value, Compare >::AVLTreeConstIterator(Tree< data_t >* rhs):
+    ptr_(rhs)
+  {
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  AVLTreeConstIterator< Key, Value, Compare >::AVLTreeConstIterator(const AVLTreeIterator< Key, Value, Compare >& rhs):
+    ptr_(rhs.ptr_)
   {
   }
 
