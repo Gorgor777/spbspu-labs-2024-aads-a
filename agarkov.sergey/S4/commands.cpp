7b0ab0fdad367a0efc112c7d3384d07c89e654a6
Gorgor777 <sergei-agarkov07@mail.ru>
2024-10-27 02:28:50 +0300

    print dict

---
+++ agarkov.sergey/S4/commands.cpp
@@ -0,0 +1,33 @@
+#include "commands.hpp"
+#include <string>
+#include <ostream>
+#include "avltree.hpp"
+
+namespace
+{
+  using dict_t = AVLTree< size_t, std::string >;
+  using main_dict = AVLTree< std::string, dict_t >;
+}
+
+void agarkov::printDict(std::string name, main_dict& dict_of_dict, std::ostream& out)
+{
+  try
+  {
+    dict_t dict = dict_of_dict.at(name);
+    if (dict.empty())
+    {
+      out << "<EMPTY>";
+      return;
+    }
+    out << name;
+    for (auto i = dict.begin(); i != dict.end(); i++)
+    {
+      out << " " << i->first << " " << i->second;
+    }
+  }
+  catch (...)
+  {
+    throw;
+  }
+}
+ 
