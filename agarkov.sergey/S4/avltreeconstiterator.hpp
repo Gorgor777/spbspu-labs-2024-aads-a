71a19283c095a6b5c45523f22197f39ce4b775f9
Gorgor777 <sergei-agarkov07@mail.ru>
2024-10-26 22:22:55 +0300

    start const iter

---
+++ agarkov.sergey/S4/avltreeconstiterator.hpp
@@ -0,0 +1,14 @@
+#ifndef AVLTREECONSTITERATOR
+#define AVLTREECONSTITERATOR
+
+namespace agarkov
+{
+  class AVLTreeConstIterator
+  {
+    public:
+      AVLTreeConstIterator();
+      ~AVLTreeConstIterator() = default;
+  };
+}
+
+#endif
