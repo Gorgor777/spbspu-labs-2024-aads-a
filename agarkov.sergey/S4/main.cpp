bc1a1c6d7d2cf8d8f316642d57e64c0fe34870aa
Gorgor777 <sergei-agarkov07@mail.ru>
2024-10-27 04:40:26 +0300

    main

--- agarkov.sergey/S4/main.cpp
+++ agarkov.sergey/S4/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <list.hpp>
 #include "avltree.hpp"
-#include "tree.hpp"
+#include "commandsdict.hpp"
+#include "inputdict.hpp"
 
-
-int main()
+int main(int argc, char* argv[])
 {
+  if (argc != 2)
+  {
+    std::cout << "Incorrect arg\n";
+    return 1;
+  }
+  std::ifstream input(argv[1]);
+  if (!input.is_open())
+  {
+    std::cout << "Error file\n";
+    return 1;
+  }
 
+  using dict_t = agarkov::AVLTree< size_t, std::string >;
+  agarkov::AVLTree< std::string, dict_t > dict_of_dict;
+  agarkov::inputDict(input, dict_of_dict);
+  agarkov::Commands dict_wiht_cmd;
+  dict_wiht_cmd.runCommand(std::cin, std::cout, dict_of_dict);
 }
+
