e8643de9da7e42a45b72773e3b84400583dec34e
Gorgor777 <sergei-agarkov07@mail.ru>
2024-10-27 04:22:49 +0300

    insert in dict

--- agarkov.sergey/S4/commandsdict.cpp
+++ agarkov.sergey/S4/commandsdict.cpp
@@ -1,1 +1,10 @@
- 
+#include "commandsdict.hpp"
+#include "commands.hpp"
+
+agarkov::Commands::Commands()
+{
+  dict1_.insert("print", printDict);
+  dict2_.insert("complement", complementDict);
+  dict2_.insert("intersect", intersectDict);
+  dict2_.insert("union", unionDict);
+}
